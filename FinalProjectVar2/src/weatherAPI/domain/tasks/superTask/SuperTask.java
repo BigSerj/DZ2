35be6a07812897eaeb7813608d40a0f6bf762683
BigSerj <sergeyconvent@gmail.com>
2017-07-26 13:25:23 +0300

    up

--- FinalProjectVar2/src/weatherAPI/domain/tasks/superTask/SuperTask.java
+++ FinalProjectVar2/src/weatherAPI/domain/tasks/superTask/SuperTask.java
@@ -5,6 +5,7 @@ import weatherAPI.data.parse.parsed.Weather;
 
 import java.util.ArrayList;
 
+
 // абстрактный класс с абстрактным методом для обработки заданий (задание 1,2,4,5)
 abstract public class SuperTask {
 
